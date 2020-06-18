e260b440d0e2ab8ac2eb382ef6d9b0fd6c7d1236
souless <clwk94@gmail.com>
2020-06-18 20:31:22 +0800

    remove static directory

--- aboutme/settings.py
+++ aboutme/settings.py
@@ -149,9 +149,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # location where django collect all static files
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
