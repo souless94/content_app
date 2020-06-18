25eca5b72b1a1d900d1fe97a99ec5d93f2cff572
souless <clwk94@gmail.com>
2020-06-18 20:24:21 +0800

    remove static root

--- aboutme/settings.py
+++ aboutme/settings.py
@@ -148,8 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # location where you will store your static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
